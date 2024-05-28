e555fc4534147c424345af69391c3819b7702946
Nilesh Kumar <131458541+NILESHSINGH234@users.noreply.github.com>
2024-05-28 20:09:46 +0530

    MVC

---
+++ Server/controllers/authControllers.js
@@ -0,0 +1,19 @@
+
+
+const signupController=async(req,res)=>{
+    try{
+
+    }catch(e){
+        console.log(e)
+    }
+}
+
+const loginController=async(req,res)=>{
+    try{
+res.send('signu')
+    }catch(e){
+        console.log(e)
+    }
+}
+
+module.exports={signupController,loginController}
--- Server/index.js
+++ Server/index.js
@@ -7,11 +7,14 @@ dotenv.config('./.env');
 let dbConnect=require('./dbConnect');
 
 
+let authRouter=require('./routers/authRouter');
 dbConnect();
 
 
 
 let app=express();
+
+app.use('/auth',authRouter)
 
 const PORT=process.env.PORT||4000;
 
